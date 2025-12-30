409244294e65f87df75da9d41384023771a94081
hakim <hakim@bareksa.com>
2025-12-30 10:51:48 +0700

    Fix: Add Solana USDC/USDT to Quote Tokens

--- offchain/normalizer.py
+++ offchain/normalizer.py
@@ -59,6 +59,8 @@ class PairNormalizer:
             '0x833589fcd6edb6e08f4c7c32d4f71b54bda02913', # Base USDC
             '0xdac17f958d2ee523a2206206994597c13d831ec7', # USDT
             '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48', # USDC
+            'epjfwdd5aufqssqem2qn1xzybapc8g4weggkzwytdt1v', # Solana USDC
+            'es9vmfrzacermjfrf4h2fyd4kconky11mcce8benwnyb', # Solana USDT
         }
         
         # If Base Token is a Quote Token, verify the other is NOT.
