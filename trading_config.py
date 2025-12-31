e9d9e9c4f9b770aa283081e5043992f42c71f32e
hakim <hakim@bareksa.com>
2025-12-31 17:28:07 +0700

    betulin trade

--- trading_config.py
+++ trading_config.py
@@ -8,7 +8,7 @@ TRADING_CONFIG = {
     # CHAIN SETTINGS (Dynamic enable/disable)
     'chains': {
         'solana': {
-            'enabled': True,
+            'enabled': False,
             'rpc_url': 'https://api.mainnet-beta.solana.com',
             'native_token': 'SOL',
             'min_native_balance': 0.01,  # Min SOL for gas
